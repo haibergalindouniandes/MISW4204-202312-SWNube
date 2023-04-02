84de13e455fd4adbc97b0b85232d030dcf23fca0
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-04-02 11:54:52 -0600

    Se agregan validaciones para parámetros opciones

--- src/mcs_tasks/api_queries.py
+++ src/mcs_tasks/api_queries.py
@@ -5,11 +5,18 @@ class TaskListResource(Resource):
     @jwt_required()
     def get(self):
         task = task_schema.dump(Task.query.all())
-        if request.json["order"] == 1:
-            result_temp = sorted(task, key=lambda d: d["id"], reverse=True)
-        else:
+        try:
+            if request.json["order"] == 1:
+                result_temp = sorted(task, key=lambda d: d["id"], reverse=True)
+            else:
+                result_temp = sorted(task, key=lambda d: d["id"], reverse=False)
+        except KeyError:
             result_temp = sorted(task, key=lambda d: d["id"], reverse=False)
-        result = result_temp[: request.json["max"]]
+
+        try:
+            result = result_temp[: request.json["max"]]
+        except KeyError:
+            result = result_temp
         return result
 
 
