3d5ee1debda84874ec60e5f776a8ebe0d2cab09d
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-05-12 18:54:07 -0500

    Implementacion startup

--- vm_web_server/mount_gcs.sh
+++ vm_web_server/mount_gcs.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-gcsfuse bucket-converter-web-app /mnt/gcs_shared
+# Validamos si existe el directorio gcs_shared 
+if [ -d "/home/gcs_shared" ] 
+then
+    echo "Directory /path/to/dir exists." 
+else
+    mkdir /home/gcs_shared
+    chmod 777 /home/gcs_shared
+fi
+# Montamos el cloud storage
+gcsfuse bucket-converter-web-app /home/gcs_shared
