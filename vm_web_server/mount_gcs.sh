50e6a853b81c2419402a15cd35925e30a2d966f6
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-05-12 19:27:08 -0500

    Implementacion startup

--- vm_web_server/mount_gcs.sh
+++ vm_web_server/mount_gcs.sh
@@ -2,10 +2,19 @@
 # Validamos si existe el directorio gcs_shared 
 if [ -d "/home/gcs_shared" ] 
 then
+    echo "Desmontando directorio /home/gcs_shared ..."
     umount /home/gcs_shared
-else
+    echo "Eliminando directorio /home/gcs_shared ..."
+    rm -rf /home/gcs_shared
+    echo "Creando directorio /home/gcs_shared ..."
     mkdir /home/gcs_shared
+    echo "Otorngando permisos al directorio /home/gcs_shared ..."
+    chmod 777 /home/gcs_shared
+else
+    echo "Creando directorio /home/gcs_shared ..."
+    mkdir /home/gcs_shared
+    echo "Otorgando permisos al directorio /home/gcs_shared ..."
     chmod 777 /home/gcs_shared
 fi
-# Montamos el cloud storage
+echo "Montando bucket en el directorio /home/gcs_shared ..."
 gcsfuse bucket-converter-web-app /home/gcs_shared
