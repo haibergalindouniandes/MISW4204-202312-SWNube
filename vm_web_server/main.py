f13bd74bd54a42953b072a3c374a9176fe3d3c6e
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-05-22 18:42:26 -0500

    Implementación Cloud Run

--- vm_web_server/main.py
+++ vm_web_server/main.py
@@ -1,7 +1,6 @@
 import os
 from flask_cors import CORS
 from flask_restful import Api
-# from flask import Flask, send_from_directory, jsonify
 from flask import Flask
 from views import AuthLogInResource, AuthSignUpResource, ConvertTaskFileResource, ConvertTaskFileByIdResource, FileDownloadResource, HealthCheckResource
 from models import db
@@ -41,5 +40,7 @@ api.add_resource(ConvertTaskFileByIdResource, "/api/tasks/<int:id_task>")
 api.add_resource(FileDownloadResource, "/api/files/<int:id_task>")
 
 
+# Inicializamos la aplicacion con Flask
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", default="80")))
+    app.run(debug=True, host="0.0.0.0", port=int(
+        os.getenv("PORT", default="80")))
